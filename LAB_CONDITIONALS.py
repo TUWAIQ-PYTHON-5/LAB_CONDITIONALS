596016738d38a6a599b8c3d80acaacbb438d7cf4
HussamAltalib <hussamaltalib@outlook.com>
2023-01-10 12:04:05 +0300

    fix logic error in line 12

--- LAB_CONDITIONALS.py
+++ LAB_CONDITIONALS.py
@@ -2,22 +2,22 @@
 movie : str = "dark_night"
 
 # - Create a variable of type int to hold the rating of the movie out of 5 . Give this movie 3
-rating : int =3
+rating : int =1
 
 # - Create a popularity score of type float , let it be 72.65
 popularity_score : float = 72.65
 
 # - Using an if statement 
 # - - Check if the movie rating is 4 or greater and the popularity is greater than 80 , print "Highly recommended"
-if rating >= 4 and popularity_score or popularity_score > 80: 
+if rating >= 4 and popularity_score > 80.0: 
     print("Highly recommended")
 
 # - - else if the movie rating is 3 or greater and the popularity is greater than 70 , print "I recommended it . It is good"
-elif rating >= 3 and popularity_score > 70:
+elif rating >= 3 and popularity_score > 70.0:
     print("I recommended it . It is good")
 
 # - - else if the movie rating is 2 or less and the popularity is greater than 60 , print "You should check it out!"
-elif rating <= 2 and popularity_score > 60:
+elif rating <= 2 and popularity_score > 60.0:
     print("You should check it out!")
 
 # -  - else  the movie rating is 2 or less and the popularity is less than 50 , print "Don't watch it. It is a waste of time"
