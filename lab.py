a3ee1c16117bff6ea3639ed552317a7c59572462
Faimubarak <102253601+Faimubarak@users.noreply.github.com>
2023-01-10 13:21:40 +0300

    Add files via upload

---
+++ lab.py
@@ -0,0 +1,18 @@
+
+fav_movie = "la la land"
+var_in : int = 3
+var_fl : float = 72.65
+
+if var_in > 4 and var_fl < 80:
+    print( "highly recomended")
+
+elif var_in >= 3 and var_fl > 70:
+    print("I recommended it . It is good")
+
+elif var_in <= 2 and var_fl > 60:
+     print("You should check it out!")
+
+elif var_in <= 2 and var_fl <50:
+      print("Don't watch it. It is a waste of time")
+
+
