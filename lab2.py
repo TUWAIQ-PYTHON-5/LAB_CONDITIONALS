828750e019cb9e7a0d4ab23f5f118270904e86ab
Mohammed <al5aldi.jav@gmail.com>
2023-01-10 11:40:26 +0300

    Lab Solved

---
+++ lab2.py
@@ -0,0 +1,21 @@
+
+
+Movie_rate = 5
+this_Movie = 3
+popularity_Score = 72.65
+
+if this_Movie >= 4 and popularity_Score > 80:
+    print("Highly recommended")
+elif this_Movie >= 3 and popularity_Score > 70:
+    print("I recommended it . It is good")
+elif this_Movie >= 2 and popularity_Score > 60:
+    print("You should check it out!")
+elif this_Movie <= 2 and popularity_Score > 50:
+    print("Don't watch it. It is a waste of time")
+
+
+
+
+
+
+
