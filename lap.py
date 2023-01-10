01c3a585404adeefae928a26cd310011bdc2a5db
qusai <qusai13255@gmail.com>
2023-01-10 11:55:42 +0300

    solve

---
+++ lap.py
@@ -0,0 +1,11 @@
+move_name = "batmane"
+raiting = 3
+popularity = 72.65
+if raiting >= 4 and popularity > 80:
+    print("Highly recommended")
+elif raiting >= 3 and popularity > 70:
+    print("I recommended it . It is good")
+elif raiting <= 2 and popularity > 60:
+    print("You should check it out!")
+else: 
+    print("Don't watch it. It is a waste of time") 
