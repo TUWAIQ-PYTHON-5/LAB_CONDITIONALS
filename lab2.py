ba5cb6e575178198f55100bbd72864336a7176a6
raghad nasser <Ra9g77@gmail.com>
2023-01-10 12:00:00 +0300

    solved lab 2

---
+++ lab2.py
@@ -0,0 +1,26 @@
+movie_1= "quiet place movie"
+movie_rating =3
+popularity_score = 72.65
+
+
+if movie_rating >= 4 and popularity_score > 80 :
+    print(movie_1 ," Highly recommended")
+
+elif movie_rating >= 3 and popularity_score > 70 :
+     print( movie_1 , "I recommended it . It is good")
+
+
+
+elif movie_rating <= 2 and popularity_score > 60 :
+     print( movie_1 ,"You should check it out!")
+
+else:
+     print( movie_1 ,"Don't watch it. It is a waste of time")
+
+
+
+
+
+
+
+
