03ff068a758023248e9b292e92ad228f05b46452
Ahmed Alhisan <alhisan.swe@gmail.com>
2023-01-10 12:01:41 +0300

    edit

--- Main.py
+++ Main.py
@@ -9,7 +9,6 @@ elif MovieRating >= 3 and MoviePopularity> 70:
     print("I recommended it . It is good")
 elif MovieRating <= 2 and MoviePopularity > 60:
     print("You should check it out!")
-elif MovieRating <= 2 and MoviePopularity < 50:
-    print("Don't watch it. It is a waste of time") 
+
 else:
-    print("please Check a real movie")    
+    print("Don't watch it. It is a waste of time")    
