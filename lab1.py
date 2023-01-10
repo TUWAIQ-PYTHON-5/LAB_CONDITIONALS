9a58c4d995e951e4aaf30cdb6b69d57e1358d764
atheer <atheeralq0@gmail.com>
2023-01-10 21:16:55 +0300

    .

--- lab1.py
+++ lab1.py
@@ -1,5 +1,5 @@
 movie = "split"
-rate : int = 3
+rate : int = 3.7
 popularity : float = 72.65
 
 if rate >= 4 and popularity > 80 :
@@ -8,9 +8,8 @@ elif rate >= 3 and popularity > 70 :
         print (movie," I recommended it . It is good")
 elif rate <= 2 and popularity > 60 :
         print (movie," You should check it out!")
-elif rate <= 2 and popularity < 50 :
-        print (movie," Don't watch it. It is a waste of time")
-else:
-    print ("find a new movie")
+else: rate <= 2 and popularity < 50 
+print (movie," Don't watch it. It is a waste of time")
+#else: print ("find a new movie")
     
 
