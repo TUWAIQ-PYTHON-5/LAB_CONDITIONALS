194891a3ece211bf97ed7a4b81ef38c2a033cb85
unknown <dalmq4806@gmail.comgit config --global user .name dalalgit config --global user.email dalmq4806@gmail.com>
2023-01-10 14:22:01 +0300

    lap.2

--- LAB_CONDITIONALS.py
+++ LAB_CONDITIONALS.py
@@ -1,14 +1,14 @@
 far_movie = "BATMAN"
 
-evaluation = int (3.5)
-evaluation2 = float = 72.65
+evaluation : int= 3.5
+evaluation2 : float = 72.65
 
 
-if evaluation >= 4 and evaluation2 > 80:
-    print(far_movie, "Highly recommend!")
+if evaluation >= 4 and evaluation2 < 80:
+    print("Highly recommend!")
 elif evaluation >= 3 and evaluation2 > 70:
-    print(far_movie,"l recommend it")
-elif evaluation <= 2 and evaluation2 >= 60:
+    print("l recommend it")
+elif evaluation <= 2 and evaluation2 > 60:
     print("You should check it out!")
 elif evaluation <= 2 and evaluation2 < 50:
     print("Don't watch it")
