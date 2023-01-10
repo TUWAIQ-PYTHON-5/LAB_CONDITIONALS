328fc1b169d0952ec691ad434fb5359950f977fc
asrar <asrar.rifae@hotmail.com>
2023-01-10 13:06:08 +0300

    modifed the varibles to Paskal csae style

--- LAB_SOLVE.py
+++ LAB_SOLVE.py
@@ -1,13 +1,13 @@
-movie = ("Inolla hlomes")
-rating :int = 3
+MovieName = ("Inolla hlomes")
+MovieRating :int = 3
 PopularityScore : float = 72.65
 
 
-if rating >=4 and PopularityScore > 80 :
+if MovieRating >=4 and PopularityScore > 80 :
     print("Highly recommended !!! ")
-elif rating >=3 and PopularityScore > 70 :
+elif MovieRating >=3 and PopularityScore > 70 :
     print(" recommended it . It is good ")
-elif rating <=2 and PopularityScore > 60 :
+elif MovieRating <=2 and PopularityScore > 60 :
     print(" You should check it out!")
 else :
     print(" Don't watch it. It is a waste of time ")
---
+++ exercises.py
@@ -0,0 +1,44 @@
+#for if statment 
+
+''' Ex 1   
+The date June 10, 1960, is special because when it is written in the following format, the month times the day equals the year:  
+6/10/60  Design a program that asks the user to enter a month (in numeric form), a day, and a two digit year. The program should then determine whether the month times the day equals the year. If so, it should display a message saying the date is magic. Otherwise, it should display a message saying the date is not magic. 
+'''
+month=int(input("enter the month:" ))
+if month >= 1 and month<=3 :
+    print("1st quarter")
+elif month >= 4 and month <=6 :
+    print("2nd quarter")
+elif month >= 7 and month <=9 :
+    print("3rd quarter")
+elif month >= 10 and month <=12 :
+    print("4th quarter")
+else :
+    print("not quarter")
+
+
+
+
+
+
+
+
+# Ex 2
+'''
+Write a program that asks the user for a month as a number between 1 and 12. The program should display a message indicating whether the month is in the first quarter, the second quarter, the third quarter, or the fourth quarter of the year.  
+Following are the guidelines: 
+If the user enters either 1, 2, or 3, the month is in the first quarter. 
+If the user enters a number between 4 and 6, the month is in the second quarter. 
+If the number is either 7, 8, or 9, the month is in the third quarter. 
+If the month is between 10 and 12, the month is in the fourth quarter.  If the number is not between 1 and 12, the program should display an error 
+
+
+'''
+mon=int(input("your moth :" ))
+day=int(input("your day :" ))
+year=int(input("your year :" ))
+if mon * day == year :
+    print("magical dat3")
+else :
+    print("not magical")
+
